76a83aa3ab18e090de143e552d5653986e35d4a1
RowanAldean <rowanaldean@gmail.com>
2020-06-15 03:39:26 +0100

    Wrote data types example file

---
+++ examples/data_types.py
@@ -0,0 +1,24 @@
+#This file shows how to declare each data type and assign it to a variable. 
+#The print statements confirm each variable type.
+
+# String
+some_string = "Janet"
+print(type(some_string))
+# Integer
+some_int_thing = 50
+print(type(some_int_thing))
+# Float
+some_float_var = 1.567
+print(type(some_float_var))
+# Boolean
+some_boolean_thing = True
+print(type(some_boolean_thing))
+# List
+some_list = ["item", 230]
+print(type(some_list))
+# Tuple
+some_tuple = (100, 'lemon')
+print(type(some_tuple))
+# Dictionary
+some_dict = {"name": "Bob"}
+print(type(some_dict))
