47bd5a6ff160c633d541568d26f75a9eafef3e6f
RowanAldean <rowanaldean@gmail.com>
2020-06-15 03:41:39 +0100

    Included a type casting example to the example data types file

--- examples/data_types.py
+++ examples/data_types.py
@@ -1,18 +1,18 @@
-#This file shows how to declare each data type and assign it to a variable. 
-#The print statements confirm each variable type.
+# This file shows how to declare each data type and assign it to a variable.
+# The print statements confirm each variable type.
 
 # String
 some_string = "Janet"
 print(type(some_string))
 # Integer
-some_int_thing = 50
-print(type(some_int_thing))
+some_int = 50
+print(type(some_int))
 # Float
-some_float_var = 1.567
-print(type(some_float_var))
+some_float = 1.567
+print(type(some_float))
 # Boolean
-some_boolean_thing = True
-print(type(some_boolean_thing))
+some_boolean = True
+print(type(some_boolean))
 # List
 some_list = ["item", 230]
 print(type(some_list))
@@ -22,3 +22,6 @@ print(type(some_tuple))
 # Dictionary
 some_dict = {"name": "Bob"}
 print(type(some_dict))
+
+# Type casting an int to a string
+print("The number " + str(some_int))
