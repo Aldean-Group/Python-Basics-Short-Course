50bab248fecfe1c793a84db72dd1bb9c3bcd7057
RowanAldean <rowanaldean@gmail.com>
2020-06-17 23:09:40 +0100

    Added fruit talk exercise model solution

--- exercises/fruit_talk.py
+++ exercises/fruit_talk.py
@@ -1,23 +1,42 @@
 import random
+
 fruits = ["banana", "apple", "pineapple", "pear", "berries"]
+
 def main_program():
+
     name = str(input("Hello, what's your name?: "))
-    print("Nice to meet you " + name) #CHALLENGE TASK (relay info)
 
-    # done_asking = False
+    print("Hello " + name)
 
-    # while(done_asking == False):
-    likesFruit = str.lower(input("Do you like fruit " + name + "? "))
+    likes_fruit = str.lower(input("Do you like fruit " + name + "?(yes/no): "))
+    while(not(likes_fruit == "yes" or likes_fruit == "no")):
+        likes_fruit = str.lower(
+            input("Do you like fruit " + name + "?(yes/no): "))
 
-    #CHALLENGE TASK
-        # if(likesFruit == "yes"):
+    if(likes_fruit == "yes"):
+        favourite_fruits = []
+        likes_random = str()  # it's a string
+        # while it isnt "stop" and the list still has choices
+        while(not(likes_random == "stop") and len(fruits) > 0):
+            random_fruit = randomFruit()  # get a random fruit
+            likes_random = str.lower(
+                input("Do you like " + random_fruit + "?(yes/no/stop): "))  # ask about it
+            if likes_random == "yes":
+                # add it to the list if they say yes
+                favourite_fruits.append(random_fruit)
 
-        # elif(likesFruit == "no")
+            # otherwise remove it to never ask again and loop back around to the top.
+            fruits.remove(random_fruit)
 
+        print("Ok we'll stop! Here's what you like: ")
+        print(favourite_fruits)
 
-    print("My favourite fruit is " + randomFruit() + " and " + randomFruit())
+    else:
+        print("Oh, wow! I like some fruits but not all of them.")
+
 
 def randomFruit():
     return random.choice(fruits)
+
 
 main_program()
